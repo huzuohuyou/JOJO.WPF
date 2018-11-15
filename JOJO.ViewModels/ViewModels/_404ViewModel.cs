14e57365e8e5ba5608e31883cf39e11d529837f8
huzuohuyou <13126506430@163.com>
2018-11-15 10:07:05 +0800

    调整项目目录

---
+++ JOJO.ViewModels/ViewModels/_404ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using JOJO.UC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOJO.ViewModels
+{
+    public class _404ViewModel : Screen, IShell
+    {
+        public _404ViewModel()
+        {
+            DisplayName = "404";
+        }
+    }
+}
